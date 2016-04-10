2aa9db5b380fd22cf0468d5f4bd2c2bc382ba616
edwinquaihoi <edwinquaihoi@gmail.com>
2016-04-10 15:26:53 +1000

    Added Unit Tests Added Log4J2 deps

--- src/main/java/au/com/anz/iib/apps/httpjsontohttpjson/PostJsonBlobTransform.java
+++ src/main/java/au/com/anz/iib/apps/httpjsontohttpjson/PostJsonBlobTransform.java
@@ -2,15 +2,14 @@ package au.com.anz.iib.apps.httpjsontohttpjson;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import au.com.anz.iib.commons.compute.IBlobTransformer;
 import au.com.anz.json.schema.httpjsontohttpjson.Company;
 import au.com.anz.json.schema.httpjsontohttpjson.User;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class PostJsonBlobTransform implements IBlobTransformer {
 
@@ -19,17 +18,18 @@ public class PostJsonBlobTransform implements IBlobTransformer {
 		//json.
 		String json = new String(input);
 
-		// trun json into pojo for processing
+		// turn json into pojo for processing
 		// expecting a collections of User objects to be returned
 		Gson gson = new Gson();		
-		Type collectionType = new TypeToken<Collection<User>>(){}.getType();
-		Collection<User> users = gson.fromJson(json, collectionType);
+		Type collectionType = new TypeToken<List<User>>(){}.getType();
+		List<User> users = gson.fromJson(json, collectionType);
 		
 		// go through the collection of users and get the country
 		List<Company> companies = new ArrayList<Company>();
-		for(User user : users) {
-			companies.add(user.getCompany());
-			System.out.println(user.toString());
+		if(users != null) {
+			for(User user : users) {
+				companies.add(user.getCompany());
+			}
 		}
 		
 		return gson.toJson(companies,new TypeToken<List<Company>>(){}.getType()).getBytes();
---
+++ src/test/java/au/com/anz/iib/apps/httpjsontohttpjson/PostJsonBlobTransformTest.java
@@ -0,0 +1,94 @@
+package au.com.anz.iib.apps.httpjsontohttpjson;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+
+import au.com.anz.json.schema.httpjsontohttpjson.Company;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class PostJsonBlobTransformTest {
+	
+	private static final Logger logger = LogManager.getLogger();	
+	private static final String TEST_FILE_001 = "user.test.001.json";
+	private static final String TEST_FILE_002 = "user.test.002.json";
+	private static final String COMPANY_NAME = "Romaguera-Crona";
+	
+	private PostJsonBlobTransform postJsonBlobTransform = new PostJsonBlobTransform();
+	private Gson gson = new Gson();
+		
+	@Test
+	public void testOnUserInput() throws IOException {
+		
+		byte[] jsonBlob = IOUtils.toByteArray(this.getClass().getResourceAsStream(TEST_FILE_001));
+		
+		// load up a json object string from file and 
+		logger.info("{} bytes read", jsonBlob.length);
+		
+		// execute transform
+		byte[] transformJsonBlob = postJsonBlobTransform.execute(jsonBlob);
+		
+		assertNotNull(transformJsonBlob);
+		
+		// convert json blob to expected Java pojo
+		gson = new Gson();
+		List<Company> companies = gson.fromJson(new String(transformJsonBlob), new TypeToken<List<Company>>(){}.getType());
+		
+		// assert list contains 1 company
+		assertEquals(1, companies.size());
+		
+		// get company and check the name
+		Company company = companies.get(0);
+		assertEquals(COMPANY_NAME, company.getName());		
+	}
+
+
+	@Test
+	public void testMultipleUsersInput() throws IOException {
+		
+		byte[] jsonBlob = IOUtils.toByteArray(this.getClass().getResourceAsStream(TEST_FILE_002));
+		
+		// load up a json object string from file and 
+		logger.info("{} bytes read", jsonBlob.length);
+		
+		// execute transform
+		byte[] transformJsonBlob = postJsonBlobTransform.execute(jsonBlob);
+		
+		assertNotNull(transformJsonBlob);
+		
+		// convert json blob to expected Java pojo
+		gson = new Gson();
+		List<Company> companies = gson.fromJson(new String(transformJsonBlob), new TypeToken<List<Company>>(){}.getType());
+		
+		// assert list contains 1 company
+		assertEquals(2, companies.size());		
+	}
+
+	@Test
+	public void testNullUserInput() throws IOException {
+		byte[] jsonBlob = new byte[]{};
+		
+		// load up a json object string from file and 
+		logger.info("{} bytes read", jsonBlob.length);
+		
+		// execute transform
+		byte[] transformJsonBlob = postJsonBlobTransform.execute(jsonBlob);
+		
+		// convert json blob to expected Java pojo
+		gson = new Gson();
+		List<Company> companies = gson.fromJson(new String(transformJsonBlob), new TypeToken<List<Company>>(){}.getType());
+		
+		// assert list contains 1 company
+		assertEquals(0, companies.size());
+	}
+
+}
